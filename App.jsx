695e04eb2b8f422556433fbfc352376814cfb540
Lisana808 <dealambert03@gmail.com>
2025-05-28 02:46:54 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Lisan+</h1>
+      <p>Welcome to your immersive Arabic experience.</p>
+    </div>
+  )
+}
